2c45e86143e3ba86babe45312783c92e46a11fc2
Raymw1 <rayan.rmw@gmail.com>
2023-09-02 18:56:57 -0300

    refactor: group all DbAddAccount protocols in a single file

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,5 +1,5 @@
 import { DbAddAccount } from './db-add-account'
-import { type Encrypter } from '../../protocols/encrypter'
+import { type Encrypter } from './db-add-account-protocols'
 
 const makeEncryter = (): Encrypter => {
   class EncrypterStub implements Encrypter {
