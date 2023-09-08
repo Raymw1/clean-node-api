cd06fe12752a99c3cda4ce16457f8f0b9d480a68
Raymw1 <rayan.rmw@gmail.com>
2023-09-08 20:49:46 -0300

    test: ensure mongodb reconnect if client is down

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -17,7 +17,7 @@ describe('Account Mongo Repository', () => {
   })
 
   beforeEach(async () => {
-    const accountsCollection = MongoHelper.getCollection('accounts')
+    const accountsCollection = await MongoHelper.getCollection('accounts')
     await accountsCollection.deleteMany({})
   })
 
