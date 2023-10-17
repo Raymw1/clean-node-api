320b92f5706722b9b139e2915e16a782023bce92
Raymw1 <rayan.rmw@gmail.com>
2023-10-16 21:07:06 -0300

    feat: ensure LoadSurveysController calls LoadSurveys

---
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -0,0 +1,13 @@
+import type { Controller, HttpRequest, HttpResponse, LoadSurveys } from './load-surveys-controller-protocols'
+
+export class LoadSurveysController implements Controller {
+  constructor (private readonly loadSurveys: LoadSurveys) { }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.loadSurveys.load()
+    return {
+      statusCode: 0,
+      body: null
+    }
+  }
+}
