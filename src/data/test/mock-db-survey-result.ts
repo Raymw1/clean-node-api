94f7f730c50cfcd3039e77d5f7c00e0fc51b35f1
Raymw1 <rayan.rmw@gmail.com>
2023-11-18 17:22:15 -0300

    refactor: move mockLoadSurveyResultRepository to a separated file

--- src/data/test/mock-db-survey-result.ts
+++ src/data/test/mock-db-survey-result.ts
@@ -1,7 +1,17 @@
+import { type LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
 import { type SaveSurveyResultRepository } from '@/data/protocols/db/survey-result/save-survey-result-repository'
 import { type SurveyResultModel } from '@/domain/models/survey-result'
 import { mockSurveyResultModel } from '@/domain/test'
 import { type SaveSurveyResultParams } from '@/domain/usecases/survey-result/save-survey-result'
+
+export const mockLoadSurveyResultRepository = (): LoadSurveyResultRepository => {
+  class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
+    async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
+      return Promise.resolve(mockSurveyResultModel())
+    }
+  }
+  return new LoadSurveyResultRepositoryStub()
+}
 
 export const mockSaveSurveyResultRepository = (): SaveSurveyResultRepository => {
   class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
