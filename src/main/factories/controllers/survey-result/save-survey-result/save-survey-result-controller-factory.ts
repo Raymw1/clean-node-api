632bc80b65223af509165e2a6925d7e2e2697ada
Raymw1 <rayan.rmw@gmail.com>
2023-11-20 16:36:13 -0300

    refactor: move saveSurveyResult factory to correct folder

--- src/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory.ts
+++ src/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory.ts
@@ -1,6 +1,6 @@
 import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
 import { makeDbLoadSurveyById } from '@/main/factories/usecases/survey/load-survey-by-id/db-load-survey-by-id-factory'
-import { makeDbSaveSurveyResult } from '@/main/factories/usecases/survey/save-survey-result/db-save-survey-result-factory'
+import { makeDbSaveSurveyResult } from '@/main/factories/usecases/survey-result/save-survey-result/db-save-survey-result-factory'
 import { SaveSurveyResultController } from '@/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller'
 import { type Controller } from '@/presentation/protocols'
 
