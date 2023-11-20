b13da249828cb3d4e1a0a3192fa9662230d52729
Raymw1 <rayan.rmw@gmail.com>
2023-11-20 15:44:50 -0300

    refactor: move LoadSurveyResultController sut creation to a factory method

---
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -0,0 +1,32 @@
+import { mockLoadSurveyById } from '@/presentation/test'
+import { LoadSurveyResultController } from './load-survey-result-controller'
+import type { HttpRequest, LoadSurveyById } from './load-survey-result-controller-protocols'
+
+const mockRequest = (): HttpRequest => ({
+  params: {
+    surveyId: 'any_id'
+  }
+})
+
+type SutTypes = {
+  loadSurveyByIdStub: LoadSurveyById
+  sut: LoadSurveyResultController
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyByIdStub = mockLoadSurveyById()
+  const sut = new LoadSurveyResultController(loadSurveyByIdStub)
+  return {
+    loadSurveyByIdStub,
+    sut
+  }
+}
+
+describe('LoadSurveyResult Controller', () => {
+  test('should call LoadSurveyById with correct value', async () => {
+    const { loadSurveyByIdStub, sut } = makeSut()
+    const loadByIdSpy = jest.spyOn(loadSurveyByIdStub, 'loadById')
+    await sut.handle(mockRequest())
+    expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
+  })
+})
