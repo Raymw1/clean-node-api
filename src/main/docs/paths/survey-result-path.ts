4b44cf3b0b50863a28c1bd260af9400654cb1e31
Raymw1 <rayan.rmw@gmail.com>
2023-11-20 16:39:56 -0300

    docs: add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -3,6 +3,7 @@ export const surveyResultPath = {
     security: [{ apiKeyAuth: [] }],
     tags: ['Survey'],
     summary: 'API to create an answer to a survey',
+    description: 'This route can only be requested by **authenticated users**',
     parameters: [{
       in: 'path',
       name: 'surveyId',
@@ -20,6 +21,35 @@ export const surveyResultPath = {
         }
       }
     },
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: { $ref: '#/components/forbidden' },
+      404: { $ref: '#/components/notFound' },
+      500: { $ref: '#/components/serverError' }
+    }
+  },
+  get: {
+    security: [{ apiKeyAuth: [] }],
+    tags: ['Survey'],
+    summary: 'API to load a survey result',
+    description: 'This route can only be requested by **authenticated users**',
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
     responses: {
       200: {
         description: 'Success',
