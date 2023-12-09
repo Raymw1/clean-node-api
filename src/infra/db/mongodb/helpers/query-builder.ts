1356296b7f8f06b0728b8c4e3d3ba813097e0387
Raymw1 <rayan.rmw@gmail.com>
2023-12-09 17:14:52 -0300

    refactor: improve query builder

--- src/infra/db/mongodb/helpers/query-builder.ts
+++ src/infra/db/mongodb/helpers/query-builder.ts
@@ -1,37 +1,38 @@
 export class QueryBuilder {
   private readonly query: object[] = []
 
+  private addStep (step: string, data: object): QueryBuilder {
+    this.query.push({
+      [step]: data
+    })
+    return this
+  }
+
   build (): object[] {
     return this.query
   }
 
   group (data: object): QueryBuilder {
-    this.query.push({ $group: data })
-    return this
+    return this.addStep('$group', data)
   }
 
   lookup (data: object): QueryBuilder {
-    this.query.push({ $lookup: data })
-    return this
+    return this.addStep('$lookup', data)
   }
 
   match (data: object): QueryBuilder {
-    this.query.push({ $match: data })
-    return this
+    return this.addStep('$match', data)
   }
 
   project (data: object): QueryBuilder {
-    this.query.push({ $project: data })
-    return this
+    return this.addStep('$project', data)
   }
 
   sort (data: object): QueryBuilder {
-    this.query.push({ $sort: data })
-    return this
+    return this.addStep('$sort', data)
   }
 
   unwind (data: object): QueryBuilder {
-    this.query.push({ $unwind: data })
-    return this
+    return this.addStep('$unwind', data)
   }
 }
